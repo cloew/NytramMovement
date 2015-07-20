a21e075d469165616a10622d2ff6c5897c0db04c
Chris Loew <cloew123@gmail.com>
2015-07-20 10:44:12 -0600

    Movement now works for all of its directions

--- nytram_movement/movement.py
+++ nytram_movement/movement.py
@@ -1,4 +1,5 @@
 from nytram.engine import Time
+from nytram.vector import Vec3
 
 class Movement:
     """ Represents Movement behavior for an Entity """
@@ -10,5 +11,5 @@ class Movement:
     
     def update(self):
         """ Update the behavior """
-        self.direction = self.directions[0].directionVector
+        self.direction = sum([direction.directionVector for direction in self.directions], Vec3(0,0,0))
         self.entity.transform.position += (self.direction*(self.speed*Time.sinceLastFrame()/1000.0))
