b3a86a01e475e0242f20bac8914be25cc6be21fe
Chris Loew <cloew123@gmail.com>
2015-07-20 09:07:07 -0600

    Added Boilerplate Package code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='nytram_movement',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['nytram_movement'],
+     )
